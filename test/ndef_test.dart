3f19386355a6dffd6dd540a88d1eba9dc29a730b
Harry Chen <i@harrychen.xyz>
2020-07-16 18:07:50 +0800

    Fix test framework
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- test/ndef_test.dart
+++ test/ndef_test.dart
@@ -3,11 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ndef/ndef.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('parse ndef message with uri type', () {
+    // TODO: fill in tests
   });
+
+  // TODO: more tests
 }
